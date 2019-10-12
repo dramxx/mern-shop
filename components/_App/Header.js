2dab711697cc1f9f9e894f60392eb8b25ac27316
Drahos Madar <drahoslav.madar@gmail.com>
2019-10-12 10:08:06 +0200

    adding menu and basic routing

---
+++ components/_App/Header.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Container, Icon, Image, Menu } from 'semantic-ui-react';
+import Link from 'next/link';
+import Router, { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
+const Header = ( props ) => {
+
+    const user = true;
+    const router = useRouter();
+
+    const isTabActive = ( route ) => {
+        return route === router.pathname;
+    };
+
+    return (
+        <Menu id={ 'menu' } fluid inverted>
+            <Container text={ true }>
+
+                <Link href={ '/' }>
+                    <Menu.Item header active={ isTabActive( '/' ) }>
+                        <Image
+                            src={ '/static/logo.svg' }
+                            size={ 'mini' }
+                            style={ { marginRight: '1em' } }
+                        />
+                        MERN | SHOP
+                    </Menu.Item>
+                </Link>
+
+                <Link href={ '/cart' }>
+                    <Menu.Item header active={ isTabActive( '/cart' ) }>
+                        <Icon
+                            name={ 'cart' }
+                            size={ 'large' }
+                        />
+                        Cart
+                    </Menu.Item>
+                </Link>
+
+                { user && (
+                    <Link href={ '/create' }>
+                        <Menu.Item header active={ isTabActive( '/create' ) }>
+                            <Icon
+                                name={ 'add square' }
+                                size={ 'large' }/>
+                            Create
+                        </Menu.Item>
+                    </Link>
+                ) }
+
+                { user ? (
+                    <>
+                        <Link href={ '/account' }>
+                            <Menu.Item header active={ isTabActive( '/account' ) }>
+                                <Icon
+                                    name={ 'user' }
+                                    size={ 'large' }
+                                />
+                                Account
+                            </Menu.Item>
+                        </Link>
+
+                        <Menu.Item header>
+                            <Icon
+                                name={ 'sign out' }
+                                size={ 'large' }
+                            />
+                            Logout
+                        </Menu.Item>
+                    </>
+                ) : (
+                    <>
+                        <Link href={ '/login' }>
+                            <Menu.Item header active={ isTabActive( '/login' ) }>
+                                <Icon
+                                    name={ 'sign in' }
+                                    size={ 'large' }
+                                />
+                                Login
+                            </Menu.Item>
+                        </Link>
+
+                        <Link href={ '/signup' }>
+                            <Menu.Item header active={ isTabActive( '/signup' ) }>
+                                <Icon
+                                    name={ 'signup' }
+                                    size={ 'large' }
+                                />
+                                Signup
+                            </Menu.Item>
+                        </Link>
+                    </>
+                ) }
+
+            </Container>
+        </Menu>
+    );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import React from "react";
 import App from "next/app";
+import Layout from "../components/_App/Layout";
 
 class MernApp extends App {
     render() {
         const { Component } = this.props;
-        return <Component/>;
+        return (
+            <Layout>
+                <Component/>
+            </Layout>
+        );
     }
 }
 
